729ee08be9f3c0391975ea42ad2beab45db3ae45
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-05-17 20:00:55 +0530

    Shortest Unique Prefix InterviewBit

---
+++ Tries/TrieQues/ShortestUniquePrefix.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class TrieNode{
+public:
+    TrieNode* children[26];
+    int pfx;
+    TrieNode(){
+        for(int i = 0; i < 26; i++){
+            children[i] = NULL;
+        }
+        pfx = 0;
+    }
+};
+
+class Trie{
+ public:
+    TrieNode* root;
+    Trie(){
+        this->root = new TrieNode();
+    }
+
+    void insert(string str){
+        TrieNode* currentNode = this->root;
+            for(int i = 0; i < str.length(); i++){
+                char letter = str[i];
+                if(currentNode->children[letter-'a'] == NULL){
+                    currentNode->children[letter-'a'] = new TrieNode();
+                }
+                currentNode = currentNode->children[letter-'a'];
+                currentNode->pfx++;
+            }
+    }
+    string findPrefix(string str){
+        TrieNode* currentNode = this->root;
+        string prefix = "";
+        int i = 0;
+        prefix+=str[i];
+        currentNode = currentNode->children[str[i] - 'a'];
+            for(int i = 1; i < str.length(); i++){
+                if(currentNode->pfx == 1){
+                    return prefix;
+                }
+                prefix+=str[i];
+                currentNode = currentNode->children[str[i] - 'a'];
+            }
+        return prefix;
+    }
+};
+//O(nS) time | O(n) space
+vector<string> prefix(vector<string> &A) {
+    Trie* trie = new Trie();
+    vector<string> result;
+    if(A.empty())
+        return result;
+    for(string word : A){
+        trie->insert(word);
+    }
+    for(string word: A){
+        string res = trie->findPrefix(word);
+        result.push_back(res);
+    }
+    return result;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<string> input;
+        for(int i = 0; i < n; i++){
+            string str;
+            cin>>str;
+            input.push_back(str);
+        }
+    vector<string> res = prefix(input);
+    for(int i = 0; i < res.size(); i++){
+        cout<< res[i] << " ";
+    }
+return 0;
+}
