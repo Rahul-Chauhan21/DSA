3c6ec863e793f3e3bcef22372cc64796c52338ef
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-05-24 08:41:04 +0530

    Added Testing Sorting Algo function

--- Sorting/quickSort.js
+++ Sorting/quickSort.js
@@ -1,4 +1,31 @@
-array = [8,5,2,9,5,6,3];
+function testSortingAlgo(){
+  for(let i = 0; i < 100; i++){
+    const array = [];
+    const length = randomIntFromInterval(1,1000);
+    for(let i = 0; i < length; i++){
+      array.push(randomIntFromInterval(-1000,1000));
+    }
+    const javaScriptSortedArray = array.slice().sort((a,b) => a - b);
+    const sortedAlgoArray = quickSort(array.slice());
+    console.log(arraysAreEqual(javaScriptSortedArray,sortedAlgoArray));
+  }
+}
+
+function randomIntFromInterval(min,max){
+  // max and min inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne,arrayTwo){
+  if(arrayOne.length !== arrayTwo.length) return false;
+  for(let i = 0; i < arrayOne.length; i++){
+    if(arrayOne[i] !== arrayTwo[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
 function quickSort(array) {
   quickSortHelper(array, 0, array.length - 1);
 	return array;
@@ -38,4 +65,32 @@ function swap(i,j,array){
 	array[i] = temp;
 }
 
-console.log(quickSort(array));
+function testSortingAlgo(){
+  for(let i = 0; i < 100; i++){
+    const array = [];
+    const length = randomIntFromInterval(1,1000);
+    for(let i = 0; i < length; i++){
+      array.push(randomIntFromInterval(-1000,1000));
+    }
+    const javaScriptSortedArray = array.slice().sort((a,b) => a - b);
+    const sortedAlgoArray = quickSort(array.slice());
+    console.log(arraysAreEqual(javaScriptSortedArray,sortedAlgoArray));
+  }
+}
+
+function randomIntFromInterval(min,max){
+  // max and min inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne,arrayTwo){
+  if(arrayOne.length !== arrayTwo.length) return false;
+  for(let i = 0; i < arrayOne.length; i++){
+    if(arrayOne[i] !== arrayTwo[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+testSortingAlgo();
