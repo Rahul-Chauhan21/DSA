78aef8ce4c87ebc19fb91cdafa1859ded5b64ae8
rahul <crahul1721@gmail.com>
2020-05-26 12:22:35 +0530

    Testing function Added

--- Sorting/quickSort.js
+++ Sorting/quickSort.js
@@ -65,32 +65,4 @@ function swap(i,j,array){
 	array[i] = temp;
 }
 
-function testSortingAlgo(){
-  for(let i = 0; i < 100; i++){
-    const array = [];
-    const length = randomIntFromInterval(1,1000);
-    for(let i = 0; i < length; i++){
-      array.push(randomIntFromInterval(-1000,1000));
-    }
-    const javaScriptSortedArray = array.slice().sort((a,b) => a - b);
-    const sortedAlgoArray = quickSort(array.slice());
-    console.log(arraysAreEqual(javaScriptSortedArray,sortedAlgoArray));
-  }
-}
-
-function randomIntFromInterval(min,max){
-  // max and min inclusive
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function arraysAreEqual(arrayOne,arrayTwo){
-  if(arrayOne.length !== arrayTwo.length) return false;
-  for(let i = 0; i < arrayOne.length; i++){
-    if(arrayOne[i] !== arrayTwo[i]){
-      return false;
-    }
-  }
-  return true;
-}
-
 testSortingAlgo();
